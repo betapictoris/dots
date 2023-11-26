f73f82b51b87ed2e95bf4d6af9f9f3f5e9728ba6
Daniel <beta@hai.haus>
2023-11-25 21:52:53 -0800

    Added yank remap

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -4,3 +4,4 @@ local tscope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', tscope.find_files, {})
 
 vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
+vim.keymap.set('n', 'yy', vim.cmd.Yank)
